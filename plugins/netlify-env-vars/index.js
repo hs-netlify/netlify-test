5a644d872a385b2f103be7f67807187d2f34bea4
Henry Smith <henry.smith@netlify.com>
2022-03-25 17:24:19 +0000

    env plugin

---
+++ plugins/netlify-env-vars/index.js
@@ -0,0 +1,16 @@
+import { processEnv } from "@next/env";
+import * as fs from "fs";
+
+module.exports = {
+  onPreBuild: () => {
+    const envs = {
+      env1: process.env.TEST_ENV_1,
+      env2: process.env.TEST_ENV_2,
+      env3: process.env.TEST_ENV_3,
+    };
+
+    let jsonEnvs = JSON.stringify(envs);
+
+    fs.writeFile("../vars/env.json", jsonEnvs, "utf8");
+  },
+};
