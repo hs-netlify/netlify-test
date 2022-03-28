09b844cb5400054b68b5d3e774e9788b33e39b5f
Henry Smith <henry.smith@netlify.com>
2022-03-28 10:07:21 +0100

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.node = {
+      fs: "empty",
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
