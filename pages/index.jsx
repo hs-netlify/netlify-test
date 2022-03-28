0cf4041608e6206297cd9605890bf9c7595aeef4
Henry Smith <henry.smith@netlify.com>
2022-03-28 10:30:30 +0100

    Try in pages

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import * as envVars from "../vars/env.js";
+import * as envVars from "./vars/env.js";
 
 console.log("Env Vars", envVars);
 
