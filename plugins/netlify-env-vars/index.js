ed047ec57c20dd9f88f1affea2e25bfb4f7c4ea3
Henry Smith <henry.smith@netlify.com>
2022-03-28 10:24:03 +0100

    Try as function

--- plugins/netlify-env-vars/index.js
+++ plugins/netlify-env-vars/index.js
@@ -2,14 +2,14 @@ const fs = require("fs");
 
 module.exports = {
   onPreBuild: () => {
-    const envs = {
-      env1: process.env.TEST_ENV_1,
-      env2: process.env.TEST_ENV_2,
-      env3: process.env.TEST_ENV_3,
-    };
-
-    let jsonEnvs = JSON.stringify(envs);
-
-    fs.writeFile("../vars/env.json", jsonEnvs, (err) => console.log(err));
+    fs.writeFile(
+      "../vars/env.json",
+      (module.exports = {
+        env1: process.env.TEST_ENV_1,
+        env2: process.env.TEST_ENV_2,
+        env3: process.env.TEST_ENV_3,
+      }),
+      (err) => console.log(err)
+    );
   },
 };
