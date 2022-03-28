2ae8d325c924e5332fedc1b53c0f92a4d2b440a8
Henry Smith <henry.smith@netlify.com>
2022-03-28 10:24:20 +0100

    Correct file name

--- plugins/netlify-env-vars/index.js
+++ plugins/netlify-env-vars/index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 module.exports = {
   onPreBuild: () => {
     fs.writeFile(
-      "../vars/env.json",
+      "../vars/env.js",
       (module.exports = {
         env1: process.env.TEST_ENV_1,
         env2: process.env.TEST_ENV_2,
